b4ade48f8a2097f0771ea30c502c0b787af57752
chriscohoat <chris.cohoat@gmail.com>
2018-09-15 16:57:57 -0600

    Added logging

--- src/grows_templates/gardeno.py
+++ src/grows_templates/gardeno.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
 import time
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 
 def main():
     while True:
+        logging.debug('Waiting for work.')
         time.sleep(5)
 
 
