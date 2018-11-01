b71ccb28b25159a8a580355fb630c0bd2817aab3
chriscohoat <chris.cohoat@gmail.com>
2018-11-01 17:41:08 -0600

    Fixed reference

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -4,7 +4,7 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from django.contrib.auth.models import Group
 
-from .models import User, LaunchSignup, Team, TeamMembership, Customer
+from .models import User, LaunchSignup, Team, TeamMembership, Customer, BetaSignup
 
 
 class UserCreationForm(forms.ModelForm):
